810956401f4673ff559e8c67c36591acdf10bb75
Sam O'Neill <sjoneill4@gmail.com>
2025-05-14 10:50:04 +0100

    Add some common compositions

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,22 @@
-// JavaScript goes here.
+(() => {
+  // Handle overflowing reel components.
+  const className = 'reel';
+  const reels = Array.from(document.querySelectorAll(`.${className}`));
+  const toggleOverflowClass = elem => {
+    elem.classList.toggle('overflowing', elem.scrollWidth > elem.clientWidth);
+  };
+
+  for (let reel of reels) {
+    if ('ResizeObserver' in window) {
+      new ResizeObserver(entries => {
+        toggleOverflowClass(entries[0].target);
+      }).observe(reel);
+    }
+
+    if ('MutationObserver' in window) {
+      new MutationObserver(entries => {
+        toggleOverflowClass(entries[0].target);
+      }).observe(reel, {childList: true});
+    }
+  }
+})();
